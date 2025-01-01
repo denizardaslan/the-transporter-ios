219d43e13e86e8f27b873a37a9eb78c7df37718d
denizardaslan <denizardaslan@gmail.com>
2025-01-01 12:03:41 +0300

    Initial Commit

---
+++ TheTransporter-Telemetry/TheTransporter_TelemetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTransporter_TelemetryApp.swift
+//  TheTransporter-Telemetry
+//
+//  Created by Deniz Arda Aslan on 1.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TheTransporter_TelemetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheTransporter-TelemetryTests/TheTransporter_TelemetryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheTransporter_TelemetryTests.swift
+//  TheTransporter-TelemetryTests
+//
+//  Created by Deniz Arda Aslan on 1.01.2025.
+//
+
+import Testing
+@testable import TheTransporter_Telemetry
+
+struct TheTransporter_TelemetryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
